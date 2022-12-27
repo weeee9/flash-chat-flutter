7bd32535ca2229480c6a867a3b0503f71d22bdf9
weeee9 <teletubby332@gmail.com>
2022-12-27 21:07:37 +0800

    chore: remove dark theme

--- lib/components/rounded_button.dart
+++ lib/components/rounded_button.dart
@@ -27,6 +27,9 @@ class RoundedButton extends StatelessWidget {
           height: 42.0,
           child: Text(
             text,
+            style: TextStyle(
+              color: Colors.white,
+            ),
           ),
         ),
       ),
